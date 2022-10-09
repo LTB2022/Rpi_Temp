65687f3d1d12d0b2d4ae601e0268edff543bd25f
LTB2022 <100973804+LTB2022@users.noreply.github.com>
2022-10-09 10:13:29 -0700

    Update simpleTkGUI.py
    This script creates a tkinter GUI with two switch buttons and an exit button. 
    The GPIO outputs correctly illuminate an LED on pins 13 & 15, NOT GPIOs 13 & 15.
    The capacitive screen correctly presses these buttons. In regard to cursor/button presses, note that screen orientation can mismatch with the hardware orientation when the screen is rotated using the adafruit-pitft.py setup script. 
    
    - Use extreme care when reading the Raspberry Pin pinout and carefully compare it with labels on any raspberry pi-breakouts, cobblers, or black-hat breakouts. The silkscreened pin labels on the older raspberry pi cobbler read "27" for pin 13, and "22" for pin 15. On the Pi400 these correspond to GPIOs 3 & 5.
    
    Next Steps:
    1. Find out how to close the tkinter window and open a new one when changing states
    2. Determine the x,y positions of a rotated GUI, find out how to correct this for any screen orientation

--- simpleTkGUI.py
+++ simpleTkGUI.py
@@ -1,4 +1,4 @@
-# File name: simpleTkGUI.py
+# File name: simpleTkGUI_Rev2.py
 # Attempting to use this working example as inspiration for the ltb
 # The code is being tested and debugged, 2022-10-09, Connor Sheeran
 
@@ -6,54 +6,62 @@
 # visit https://robu.in for more information
 # https://robu.in/developing-simple-gui-using-tkinter-to-control-gpios-of-raspberry-pi/
 
-import tkinter as Tk   # importing Tkinter Library
-# import tkFont
-import RPi.GPIO as GPIO   # importing GPIO library
+
+import tkinter as tk
+from tkinter import *
+from tkinter import Tk, Label, Button, font
+import time
+import sys
+import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)  # setting up the GPIO pins
-GPIO.setup(37, GPIO.OUT)  # setting pin37 as output pin
-GPIO.output(37, GPIO.LOW)  # setting pin37 as LOW initially
-GPIO.setup(38, GPIO.OUT)  # setting pin38 as output pin
-GPIO.output(38, GPIO.LOW)  # setting pin38 as LOW initially
+# Setting up LED1
+GPIO.setup(13, GPIO.OUT)  # setting pin13 as output pin NOT GPIO 13
+GPIO.output(13, GPIO.LOW)  # setting pin13 as LOW initially
+# Setting up LED2
+GPIO.setup(15, GPIO.OUT)  # setting pin23 as output pin NOT GPIO 155
+GPIO.output(15, GPIO.LOW)  # setting pin23 as LOW initially
 
 win = Tk()  # setting up window from Tk() naming win
 
-# myFont = tkFont.Font(family='Helvetica', size=36, weight='bold')  # setting up font naming myfont
+myFont = font.Font(family='Helvetica', size=36, weight='bold')  # setting up font naming myfont
 
-def led1ON():  # defining function led1ON
-    print("LED1 button pressed")  # to be printed on terminal
-    if GPIO.input(37) :
-        GPIO.output(37, GPIO.LOW)  # setting pin37 to low
-        led1Button["text"] = "LED 1 ON"  # text for led1Button
-
+def SW1ON():  # defining function led1ON
+    print("SW1 button pressed")  # to be printed on terminal
+    if GPIO.input(13) :
+        GPIO.output(13, GPIO.LOW)  # setting pin13 to low
+        SW1Button["text"] = "SW 1 OFF"  # text for led1Button
     else:
-        GPIO.output(37, GPIO.HIGH)  # setting pin37 to high
-        led1Button["text"] = "LED 1 OFF"  # text for led1Button
+        GPIO.output(13, GPIO.HIGH)  # setting pin13 to high
+        SW1Button["text"] = "SW 1 ON"  # text for led1Button
 
-def led2ON():
-    print("LED2 button pressed")
-    if GPIO.input(38) :
-        GPIO.output(38, GPIO.LOW)  # setting pin38 to low
-        led2Button["text"] = "LED 2 ON"  # text for led2Button
+def SW2ON():
+    print("SW2 button pressed")
+    if GPIO.input(15) :
+        GPIO.output(15, GPIO.LOW)  # setting pin15 to low
+        SW2Button["text"] = "SW 2 OFF"  # text for led2Button
     else:
-        GPIO.output(38, GPIO.HIGH)  # setting pin38 to high
-        led2Button["text"] = "LED 2 OFF"  # text for led2Button
+        GPIO.output(15, GPIO.HIGH)  # setting pin15 to high
+        SW2Button["text"] = "SW 2 ON"  # text for led2Button
 
 def exitProgram():
     print("Exit Button pressed")
     GPIO.cleanup()  # Quitting program
     win.quit()
 
-win.title("My GUI")  # title for window
-win.geometry('800x300')  # Dimensions of the window
+# Define the tkinter window instance
+win.title("ltb Home")  # title for window
+win.geometry('800x500')  # Dimensions of the window, 320x240 is the dimensions of the adafruit PiTFT capacitive screen
+win.eval('tk::PlaceWindow . center')  #Place the window in the center of the screen, Q: is the Raspberry Screen setup correctly?
 
-exitButton = Button(win, text="Exit", font=myFont, command=exitProgram, height=2 , width=6)  # setting button naming exitbutton 
-exitButton.pack(side=BOTTOM)  # button position for exitbutton #commanding to button to exitProgram
+# Define the tkinter button instance(s)
+exitButton = Button(win, text="Exit", font=myFont, command=exitProgram, height=2 , width=6)  # setting button naming exitbutton
+exitButton.pack(side=BOTTOM, anchor=SE)  # button position for exitbutton #commanding to button to exitProgram
 
-led1Button = Button(win, text="LED 1 ON", font=myFont, command=led1ON, height=2, width=8)  # setting button naming led1Button
-led1Button.place(x=37, y=50)  # button position for led1Button #commanding to button to led1ON function
+SW1Button = Button(win, text="SW 1 OFF", font=myFont, command=SW1ON, height=2, width=8)  # setting button naming led1Button
+SW1Button.place(x=37, y=50)  # button position for led1Button #commanding to button to led1ON function
 
-led2Button = Button(win, text="LED 2 ON", font=myFont, command=led2ON, height=2, width=8)  # setting button naming led2Button
-led2Button.place(x=520, y=50)  # button position for led2Button #commanding to button to led2ON function
+SW2Button = Button(win, text="SW 2 OFF", font=myFont, command=SW2ON, height=2, width=8)  # setting button naming led2Button
+SW2Button.place(x=520, y=50)  # button position for led2Button #commanding to button to led2ON function
 
 mainloop()  # commanding mainloop for starting main loop
